74c7f33fcf195b0e3c038dca4eb816e170f8baac
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-19 12:12:01 +0200

    Add select question group viewcontroller

---
+++ RabbleWabble/RabbleWabble/Controllers/SelectQuestionGroupViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SelectQuestionGroupViewController.swift
+//  RabbleWabble
+//
+//  Created by Vlad Stanescu on 19.01.2021.
+//
+
+import UIKit
+
+public class SelectQuestionGroupViewController: UIViewController {
+    
+    // MARK: - Outlets
+    @IBOutlet internal var tableView: UITableView! {
+        didSet {
+            tableView.tableFooterView = UIView()
+        }
+    }
+    
+    // MARK: - Properties
+    public let questionGroups = QuestionGroup.allGroups()
+    private var selectedQuestionGroup: QuestionGroup!
+}
+
+extension SelectQuestionGroupViewController: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questionGroups.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "QuestionGroupCell") as! QuestionGroupCell
+        let questionGroup = questionGroups[indexPath.row]
+        cell.titleLabel.text = questionGroup.title
+        return cell
+    }
+}
---
+++ RabbleWabble/RabbleWabble/Views/QuestionGroupCell.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionGroupCell.swift
+//  RabbleWabble
+//
+//  Created by Vlad Stanescu on 19.01.2021.
+//
+
+import UIKit
+
+public class QuestionGroupCell: UITableViewCell {
+    @IBOutlet public var titleLabel: UILabel!
+    @IBOutlet public var percentageLabel: UILabel!
+}
