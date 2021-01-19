72828a2d554613da4c6e014615cdc3e2d02fcd4a
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-19 09:33:44 +0200

    Move gitignore

---
+++ RabbleWabble/RabbleWabble/Models/Question.swift
@@ -0,0 +1,8 @@
+//
+//  Question.swift
+//  RabbleWabble
+//
+//  Created by Vlad Stanescu on 19.01.2021.
+//
+
+import Foundation
---
+++ RabbleWabble/RabbleWabble/Models/QuestionGroup.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionGroup.swift
+//  RabbleWabble
+//
+//  Created by Vlad Stanescu on 19.01.2021.
+//
+
+import Foundation
