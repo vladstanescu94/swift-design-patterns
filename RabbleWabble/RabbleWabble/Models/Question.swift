00272b82f8fb689617780eeb4fea28b3c0627a3d
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-19 09:35:58 +0200

    Add model files

--- RabbleWabble/RabbleWabble/Models/Question.swift
+++ RabbleWabble/RabbleWabble/Models/Question.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+public struct Question {
+    public let answer: String
+    public let hint: String?
+    public let prompt: String
+}
--- RabbleWabble/RabbleWabble/Models/QuestionGroup.swift
+++ RabbleWabble/RabbleWabble/Models/QuestionGroup.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+public struct QuestionGroup {
+    public let questions: [Question]
+    public let title: String
+}
