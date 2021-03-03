fe4813a4ddc4eab2a26d176542b78edefcbacc8a
Vlad Stanescu <vladstanescu94@gmail.com>
2021-03-03 11:32:37 +0200

    Add adapter implementation

--- Intermediate Design Patterns/Adapter/CoffeeQuest/CoffeeQuest/Adapters/YLPClient+BusinessSearchClient.swift
+++ Intermediate Design Patterns/Adapter/CoffeeQuest/CoffeeQuest/Adapters/YLPClient+BusinessSearchClient.swift
@@ -26,4 +26,40 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import Foundation
+import MapKit
+import YelpAPI
+
+extension YLPClient: BusinessSearchClient {
+  public func search(with coordinate: CLLocationCoordinate2D, term: String, limit: UInt, offset: UInt, success: @escaping (([Business]) -> Void), failure: @escaping ((Error?) -> Void)) {
+    
+    let yelpCoordinate = YLPCoordinate(latitude: coordinate.latitude, longitude: coordinate.longitude)
+    
+    search(with: yelpCoordinate,
+           term: term,
+           limit: limit,
+           offset: offset,
+           sort: .bestMatched) { (searchResult, error) in
+      
+      guard let searchResult = searchResult,
+            error == nil else {
+        failure(error)
+        return
+      }
+      
+      let business = searchResult.businesses.adaptToBusinesses()
+      success(business)
+    }
+  }
+}
+
+extension Array where Element: YLPBusiness {
+  func adaptToBusinesses() -> [Business] {
+    return compactMap { yelpBusiness in
+      guard let yelpCoordinate = yelpBusiness.location.coordinate else { return nil }
+      
+      let coordinate = CLLocationCoordinate2D(latitude: yelpCoordinate.latitude, longitude: yelpCoordinate.longitude)
+      
+      return Business(name: yelpBusiness.name, rating: yelpBusiness.rating, location: coordinate)
+    }
+  }
+}
