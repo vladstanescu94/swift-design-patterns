1c10a73f2a9ab285a126a4d4d34a70b95192eedf
Vlad Stanescu <vladstanescu94@gmail.com>
2021-05-12 11:14:28 +0300

    Add AcceptInputState logic

--- Intermediate Design Patterns/State/MirrorPad/MirrorPad/Views/DrawView/States/AcceptInputState.swift
+++ Intermediate Design Patterns/State/MirrorPad/MirrorPad/Views/DrawView/States/AcceptInputState.swift
@@ -29,5 +29,35 @@
 import UIKit
 
 public class AcceptInputState: DrawViewState {
+  public override func animate() {
+    let animateState = transitionToState(matching: AnimateState.identifier)
+    animateState.animate()
+  }
   
+  public override func clear() {
+    let clearState = transitionToState(matching: ClearState.identifier)
+    clearState.clear()
+  }
+  
+  public override func copyLines(from source: DrawView) {
+    let copyState = transitionToState(matching: CopyState.identifier)
+    copyState.copyLines(from: source)
+  }
+  
+  public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let point = touches.first?.location(in: drawView) else {
+      return
+    }
+    
+    let line = LineShape(color: drawView.lineColor, width: drawView.lineWidth, startPoint: point)
+    drawView.lines.append(line)
+    drawView.layer.addSublayer(line)
+  }
+  
+  public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let point = touches.first?.location(in: drawView),
+          drawView.bounds.contains(point),
+          let currentLine = drawView.lines.last else { return }
+    currentLine.addPoint(point)
+  }
 }
