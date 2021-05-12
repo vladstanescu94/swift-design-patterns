cb6f04fb6f1e649231ff7492276bd3479b7580c4
Vlad Stanescu <vladstanescu94@gmail.com>
2021-05-12 11:28:06 +0300

    Add AnimateState implementation

--- Intermediate Design Patterns/State/MirrorPad/MirrorPad/Views/DrawView/DrawView.swift
+++ Intermediate Design Patterns/State/MirrorPad/MirrorPad/Views/DrawView/DrawView.swift
@@ -56,61 +56,16 @@ public class DrawView: UIView {
 
   // MARK: - UIResponder
   public override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    guard let point = touches.first?.location(in: self) else { return }
-    let line = LineShape(color: lineColor, width: lineWidth, startPoint: point)
-    lines.append(line)
-    layer.addSublayer(line)
+    currentState.touchesBegan(touches, with: event)
   }
 
   public override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-    guard let point = touches.first?.location(in: self),
-      bounds.contains(point),
-      let currentLine = lines.last else { return }
-    currentLine.addPoint(point)
+    currentState.touchesMoved(touches, with: event)
   }
 
   // MARK: - Actions
   public func animate() {
-    guard let sublayers = layer.sublayers, sublayers.count > 0 else { return }
-    sublayers.forEach { $0.removeAllAnimations() }
-    UIView.animate(withDuration: 0.3) {
-      CATransaction.begin()
-      self.setSublayersStrokeEnd(to: 0.0)
-      self.animateStrokeEnds(of: sublayers, at: 0)
-      CATransaction.commit()
-    }
-  }
-
-  private func setSublayersStrokeEnd(to value: CGFloat) {
-    layer.sublayers?.forEach {
-      guard let shapeLayer = $0 as? CAShapeLayer else { return }
-      shapeLayer.strokeEnd = 0.0
-      let animation = CABasicAnimation(keyPath: "strokeEnd")
-      animation.fromValue = value
-      animation.toValue = value
-      animation.fillMode = .forwards
-      shapeLayer.add(animation, forKey: nil)
-    }
-  }
-
-  private func animateStrokeEnds(of layers: [CALayer], at index: Int) {
-    guard index < layers.count else { return }
-    let currentLayer = layers[index]
-    CATransaction.begin()
-    CATransaction.setCompletionBlock { [weak self] in
-      currentLayer.removeAllAnimations()
-      self?.animateStrokeEnds(of: layers, at: index + 1)
-    }
-    if let shapeLayer = currentLayer as? CAShapeLayer {
-      shapeLayer.strokeEnd = 1.0
-      let animation = CABasicAnimation(keyPath: "strokeEnd")
-      animation.duration = 1.0
-      animation.fillMode = .forwards
-      animation.fromValue = 0.0
-      animation.toValue = 1.0
-      shapeLayer.add(animation, forKey: nil)
-    }
-    CATransaction.commit()
+    currentState.animate()
   }
 
   public func clear() {
--- Intermediate Design Patterns/State/MirrorPad/MirrorPad/Views/DrawView/States/AnimateState.swift
+++ Intermediate Design Patterns/State/MirrorPad/MirrorPad/Views/DrawView/States/AnimateState.swift
@@ -29,5 +29,54 @@
 import UIKit
 
 public class AnimateState: DrawViewState {
+  public override func animate() {
+    guard let sublayers = drawView.layer.sublayers,
+          sublayers.count > 0 else {
+      transitionToState(matching: AcceptInputState.identifier)
+      return
+    }
+    
+    sublayers.forEach { $0.removeAllAnimations() }
+    UIView.animate(withDuration: 0.3) {
+      CATransaction.begin()
+      CATransaction.setCompletionBlock { [weak self] in
+        self?.transitionToState(matching: AcceptInputState.identifier)
+      }
+      self.setSublayersStrokeEnd(to: 0.0)
+      self.animateStrokeEnds(of: sublayers, at: 0)
+      CATransaction.commit()
+    }
+  }
   
+  private func setSublayersStrokeEnd(to value: CGFloat) {
+    drawView.layer.sublayers?.forEach {
+      guard let shapeLayer = $0 as? CAShapeLayer else { return }
+      shapeLayer.strokeEnd = 0.0
+      let animation = CABasicAnimation(keyPath: "strokeEnd")
+      animation.fromValue = value
+      animation.toValue = value
+      animation.fillMode = .forwards
+      shapeLayer.add(animation, forKey: nil)
+    }
+  }
+  
+  private func animateStrokeEnds(of layers: [CALayer], at index: Int) {
+    guard index < layers.count else { return }
+    let currentLayer = layers[index]
+    CATransaction.begin()
+    CATransaction.setCompletionBlock { [weak self] in
+      currentLayer.removeAllAnimations()
+      self?.animateStrokeEnds(of: layers, at: index + 1)
+    }
+    if let shapeLayer = currentLayer as? CAShapeLayer {
+      shapeLayer.strokeEnd = 1.0
+      let animation = CABasicAnimation(keyPath: "strokeEnd")
+      animation.duration = 1.0
+      animation.fillMode = .forwards
+      animation.fromValue = 0.0
+      animation.toValue = 1.0
+      shapeLayer.add(animation, forKey: nil)
+    }
+    CATransaction.commit()
+  }
 }
