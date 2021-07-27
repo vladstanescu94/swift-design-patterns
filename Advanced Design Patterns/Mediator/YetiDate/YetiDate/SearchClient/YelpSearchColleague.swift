39ba73b5ff25a15c33c7a53da6e30c77b10e3f06
Vlad Stanescu <vladstanescu94@gmail.com>
2021-07-27 11:32:39 +0300

    Add Yelp Search Colleague

--- Advanced Design Patterns/Mediator/YetiDate/YetiDate/SearchClient/YelpSearchColleague.swift
+++ Advanced Design Patterns/Mediator/YetiDate/YetiDate/SearchClient/YelpSearchColleague.swift
@@ -25,3 +25,87 @@
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
+
+import CoreLocation
+import YelpAPI
+
+public class YelpSearchColleague {
+  public let category: YelpCategory
+  public private(set) var selectedBusiness: YLPBusiness?
+  
+  private var colleagueCoordinate: CLLocationCoordinate2D?
+  private unowned let mediator: SearchColleagueMediating
+  private var userCoordinate: CLLocationCoordinate2D?
+  private let yelpClient: YLPClient
+  
+  private static let defaultQueryLimit = UInt(20)
+  private static let defaultQuerySort = YLPSortType.bestMatched
+  private var queryLimit = defaultQueryLimit
+  private var querySort = defaultQuerySort
+  
+  public init(category: YelpCategory, mediator: SearchColleagueMediating) {
+    self.category = category
+    self.mediator = mediator
+    self.yelpClient = YLPClient(apiKey: YelpAPIKey)
+  }
+}
+
+extension YelpSearchColleague: SearchColleague {
+  public func update(userCoordinate: CLLocationCoordinate2D) {
+    self.userCoordinate = userCoordinate
+    performSearch()
+  }
+  
+  public func fellowColleague(_ colleague: SearchColleague, didSelect business: YLPBusiness) {
+    colleagueCoordinate = CLLocationCoordinate2D(business.location.coordinate)
+    queryLimit /= 2
+    querySort = .distance
+    performSearch()
+  }
+  
+  public func reset() {
+    colleagueCoordinate = nil
+    queryLimit = YelpSearchColleague.defaultQueryLimit
+    querySort = YelpSearchColleague.defaultQuerySort
+    selectedBusiness = nil
+    performSearch()
+  }
+  
+  private func performSearch() {
+    guard selectedBusiness == nil,
+          let coordinate = colleagueCoordinate ?? userCoordinate else { return }
+    
+    let yelpCoordinate = YLPCoordinate(
+      latitude: coordinate.latitude,
+      longitude: coordinate.longitude)
+    
+    let query = YLPQuery(coordinate: yelpCoordinate)
+    query.categoryFilter = [category.rawValue]
+    query.limit = queryLimit
+    query.sort = querySort
+    
+    yelpClient.search(with: query) { [weak self] (search, error) in
+      guard let self = self else { return }
+      guard let search = search else {
+        self.mediator.searchColleague(self, searchFailed: error)
+        return
+      }
+      
+      var set: Set<BusinessMapViewModel> = []
+      for business in search.businesses {
+        guard let coordinate = business.location.coordinate else { continue }
+        
+        let viewModel = BusinessMapViewModel(business: business, coordinate: coordinate, primaryCategory: self.category) { [weak self] business in
+          guard let self = self else { return }
+          self.selectedBusiness = business
+          self.mediator.searchColleague(self, didSelect: business)
+        }
+        set.insert(viewModel)
+      }
+      
+      DispatchQueue.main.async {
+        self.mediator.searchColleague(self, didCreate: set)
+      }
+    }
+  }
+}
